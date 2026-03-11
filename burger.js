3930b7e9d3c3e0acaa297329102052fbdded0aba
Cloudier-Awais <awaisanjum8086@gmail.com>
2026-03-11 14:33:30 +0500

    Replace burger with animated ice cream

--- burger.js
+++ burger.js
@@ -1,15 +1,15 @@
-const burger = document.getElementById('burger');
+const icecream = document.getElementById('icecream');
 const disBtn = document.getElementById('disassembleBtn');
 const asmBtn = document.getElementById('assembleBtn');
 
 disBtn.addEventListener('click', () => {
-  burger.classList.add('disassembled');
+  icecream.classList.add('disassembled');
   disBtn.style.display = 'none';
   asmBtn.style.display = 'inline-block';
 });
 
 asmBtn.addEventListener('click', () => {
-  burger.classList.remove('disassembled');
+  icecream.classList.remove('disassembled');
   asmBtn.style.display = 'none';
   disBtn.style.display = 'inline-block';
 });
